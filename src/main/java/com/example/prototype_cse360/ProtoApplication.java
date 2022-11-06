1f77b8554fc9f78ca7e5519d66bf10492ab75cdc
Nicholaskarl <nkjohns6@asu.edu>
2022-11-05 17:18:41 -0700

    Chef and Order Processing Agent UI with helper classes to allow it to run and demonstrate usefullness.

--- src/main/java/com/example/prototype_cse360/ProtoApplication.java
+++ src/main/java/com/example/prototype_cse360/ProtoApplication.java
@@ -13,6 +13,7 @@ import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class ProtoApplication extends Application {
@@ -21,6 +22,11 @@ public class ProtoApplication extends Application {
 
     private final FoodItem[] foodItems = Utils.getFoodItems();
 
+    private TempSupportClass caller= new TempSupportClass();
+    private  final ArrayList<OrderListHelper> orders = caller.listOShit();
+
+    private int added=0;
+    
     /**
      * start
      *
@@ -73,7 +79,20 @@ public class ProtoApplication extends Application {
      * @return The main scene for the chef
      * */
 
-    private Scene chefScene() { return new Scene(new Label("chefScene"));}
+    private Scene chefScene() {
+        added++;
+        ChefHelper ch= new ChefHelper(orders, added);
+        VBox mainBox = new VBox();
+
+        Button routingButton = new Button("to OPAgent");
+        routingButton.setOnAction(event -> {primaryStage.setScene(orderManagerScene());});
+
+        mainBox.getChildren().addAll(ch.TopBox(),ch.OrderList(), routingButton);
+
+        Scene scene=new Scene(mainBox,1000,1200);
+        return scene;
+        //return new Scene(new Label("chefScene"));
+    }
 
     /**
      * customerScene
@@ -234,6 +253,18 @@ public class ProtoApplication extends Application {
      * @return The main scene for the order manager
      * */
 
-    private Scene orderManagerScene() {return new Scene(new Label("orderManagerScene"));}
+    private Scene orderManagerScene() { added++;
+        OPHelper ch= new OPHelper(orders, added);
+        VBox mainBox = new VBox();
+        Button routingButton = new Button("To CHef");
+        routingButton.setOnAction(event -> {primaryStage.setScene(chefScene());});
+
+
+        mainBox.getChildren().addAll(ch.ColumnTitles(), ch.OrderList(), routingButton );
+
+        Scene scene=new Scene(mainBox,1000,1200);
+        return scene;
+        //return new Scene(new Label("orderManagerScene"));
+    }
 
 }
---
+++ src/main/java/com/example/prototype_cse360/TempSupportClass.java
@@ -0,0 +1,15 @@
+package com.example.prototype_cse360;
+
+import java.util.ArrayList;
+
+public class TempSupportClass {
+    OrderListHelper oLHelper = new OrderListHelper();
+    public ArrayList<OrderListHelper> listOShit(){
+        ArrayList<OrderListHelper> orders = new ArrayList<>();
+        for(int i=0; i<5; i++){
+            orders.add(new OrderListHelper());
+        }
+        return orders;
+    }
+    
+}
